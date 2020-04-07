821b3f50eb2d33b632ad5138a183eda239e6a40e
Chris Moreton <chris.moreton@hmcts.net>
2020-04-07 13:33:34 +0100

    Tagging as 7.0.1

--- src/main/java/com/netsensia/rivalchess/model/Square.kt
+++ src/main/java/com/netsensia/rivalchess/model/Square.kt
@@ -7,8 +7,71 @@ import com.netsensia.rivalchess.model.helper.SliderDirection
 import com.netsensia.rivalchess.model.util.CommonUtils
 import java.util.*
 
-enum class Square(val xFile: Int, val yRank: Int) {
-    A8(0, 0), B8(1, 0), C8(2, 0), D8(3, 0), E8(4, 0), F8(5, 0), G8(6, 0), H8(7, 0), A7(0, 1), B7(1, 1), C7(2, 1), D7(3, 1), E7(4, 1), F7(5, 1), G7(6, 1), H7(7, 1), A6(0, 2), B6(1, 2), C6(2, 2), D6(3, 2), E6(4, 2), F6(5, 2), G6(6, 2), H6(7, 2), A5(0, 3), B5(1, 3), C5(2, 3), D5(3, 3), E5(4, 3), F5(5, 3), G5(6, 3), H5(7, 3), A4(0, 4), B4(1, 4), C4(2, 4), D4(3, 4), E4(4, 4), F4(5, 4), G4(6, 4), H4(7, 4), A3(0, 5), B3(1, 5), C3(2, 5), D3(3, 5), E3(4, 5), F3(5, 5), G3(6, 5), H3(7, 5), A2(0, 6), B2(1, 6), C2(2, 6), D2(3, 6), E2(4, 6), F2(5, 6), G2(6, 6), H2(7, 6), A1(0, 7), B1(1, 7), C1(2, 7), D1(3, 7), E1(4, 7), F1(5, 7), G1(6, 7), H1(7, 7);
+enum class Square(val xFile: Int, val yRank: Int, val bitRef: Int) {
+    A8(0, 0, 0),
+    B8(1, 0, 1),
+    C8(2, 0, 2),
+    D8(3, 0, 3),
+    E8(4, 0, 4),
+    F8(5, 0, 5),
+    G8(6, 0, 6),
+    H8(7, 0, 7),
+    A7(0, 1, 8),
+    B7(1, 1, 9),
+    C7(2, 1, 10),
+    D7(3, 1, 11),
+    E7(4, 1, 12),
+    F7(5, 1, 13),
+    G7(6, 1, 14),
+    H7(7, 1, 15),
+    A6(0, 2, 16),
+    B6(1, 2, 17),
+    C6(2, 2, 18),
+    D6(3, 2, 19),
+    E6(4, 2, 20),
+    F6(5, 2, 21),
+    G6(6, 2, 22),
+    H6(7, 2, 23),
+    A5(0, 3, 24),
+    B5(1, 3, 25),
+    C5(2, 3, 26),
+    D5(3, 3, 27),
+    E5(4, 3, 28),
+    F5(5, 3, 29),
+    G5(6, 3, 30),
+    H5(7, 3, 31),
+    A4(0, 4, 32),
+    B4(1, 4, 33),
+    C4(2, 4, 34),
+    D4(3, 4, 35),
+    E4(4, 4, 36),
+    F4(5, 4, 37),
+    G4(6, 4, 38),
+    H4(7, 4, 39),
+    A3(0, 5, 40),
+    B3(1, 5, 41),
+    C3(2, 5, 42),
+    D3(3, 5, 43),
+    E3(4, 5, 44),
+    F3(5, 5, 45),
+    G3(6, 5, 46),
+    H3(7, 5, 47),
+    A2(0, 6, 48),
+    B2(1, 6, 49),
+    C2(2, 6, 50),
+    D2(3, 6, 51),
+    E2(4, 6, 52),
+    F2(5, 6, 53),
+    G2(6, 6, 54),
+    H2(7, 6, 55),
+    A1(0, 7, 56),
+    B1(1, 7, 57),
+    C1(2, 7, 58),
+    D1(3, 7, 59),
+    E1(4, 7, 60),
+    F1(5, 7, 61),
+    G1(6, 7, 62),
+    H1(7, 7, 63);
 
     companion object {
         private val validSliderDirections: MutableMap<Square, MutableList<SliderDirection>> = EnumMap(Square::class.java)
@@ -25,8 +88,8 @@ enum class Square(val xFile: Int, val yRank: Int) {
         }
 
         @kotlin.jvm.JvmStatic
-        fun fromCoords(x: Int, y: Int): Square {
-            return when (y * 8 + x) {
+        fun fromBitRef(bitRef: Int): Square {
+            return when (bitRef) {
                 0 -> A8
                 1 -> B8
                 2 -> C8
@@ -91,7 +154,16 @@ enum class Square(val xFile: Int, val yRank: Int) {
                 61 -> F1
                 62 -> G1
                 63 -> H1
-                else -> throw EnumConversionException("Invalid Square conversion")
+                else -> throw EnumConversionException("Invalid BitRef for Square: ${bitRef}")
+            }
+        }
+
+        @kotlin.jvm.JvmStatic
+        fun fromCoords(x: Int, y: Int): Square {
+            try {
+                return fromBitRef(y * 8 + x)
+            } catch (e: EnumConversionException) {
+                throw EnumConversionException("Invalid Coords for Square: ($x,$y)")
             }
         }
 
